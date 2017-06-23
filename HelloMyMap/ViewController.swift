a3acd6c08cfb6bf109a4c9c353195e73b6c94631
fguai <fguai@fguais-iMac-9.local>
2017-06-23 10:37:10 +0800

    Initial Commit

---
+++ HelloMyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloMyMap
+//
+//  Created by fguai on 2017/6/23.
+//  Copyright © 2017年 xuhaojun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
